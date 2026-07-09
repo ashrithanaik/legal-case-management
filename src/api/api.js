f9850a36cf0c9c4541d2b7e172378787dce01c9d
Akshaya Koduri <akshayakoduri3096@gmail.com>
2026-07-09 23:15:25 +0530

    Connect frontend to Render backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://legal-case-management-mzi6.onrender.com",
 });
 
 export default api;
